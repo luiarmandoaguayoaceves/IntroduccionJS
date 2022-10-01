24b95dda0bcd0d41fbc55a39984ee3cdc8df26c2
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2022-09-30 20:38:41 -0500

    30 sept

---
+++ js/06.js
@@ -0,0 +1,14 @@
+//Objeto Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.2);
+resultado = Math.ceil(2.2);//redondear hacia arriba
+resultado = Math.floor(2.1);//redondear hacia abajo
+resultado = Math.sqrt(144);
+resultado = Math.abs(-200);
+
+
+
+console.log(resultado);
